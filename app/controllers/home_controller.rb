58e4fb3f1caa5a18918a4f7211e4ba2497bd2c9f
Bartosz Krajka <krajka.bartosz@gmail.com>
2018-09-19 20:35:12 +0200

    Add Catan gem and present data on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
   def index
+    players = [
+      { name: 'Bartek', color: :orange },
+      { name: 'John', color: :red }
+    ]
+    response = SetupGame.new(players: players).invoke
+
+    @catan = Catan.new
+    @catan.map = response[:map]
+    @catan.players = response[:players]
+    @catan.current_player = response[:current_player]
+    @catan.turn = response[:turn]
   end
 end

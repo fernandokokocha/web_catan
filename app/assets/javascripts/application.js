89b65ab1ed1b2114c4b4c70ef89b7055ecc71c3c
Bartosz Krajka <krajka.bartosz@gmail.com>
2018-09-19 21:20:32 +0200

    Add very simple map representation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,52 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+const drawField = ([baseX, baseY], color) => {
+  const c = document.getElementById("map");
+  const ctx = c.getContext("2d");
+
+  ctx.beginPath();
+  ctx.moveTo(baseX + 30, baseY);
+  ctx.lineTo(baseX + 80, baseY);
+  ctx.lineTo(baseX + 110, baseY + 40);
+  ctx.lineTo(baseX + 80, baseY + 80);
+  ctx.lineTo(baseX + 30, baseY + 80);
+  ctx.lineTo(baseX, baseY + 40);
+  ctx.lineTo(baseX + 30, baseY);
+  ctx.stroke();
+
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
+const dimsForField = [
+  [160, 160],
+  [160, 80],
+  [240, 120],
+  [240, 200],
+  [160, 240],
+  [80, 200],
+  [80, 120],
+  [160, 0],
+  [240, 40],
+  [320, 80],
+  [320, 160],
+  [320, 240],
+  [240, 280],
+  [160, 320],
+  [80, 280],
+  [0, 240],
+  [0, 160],
+  [0, 80],
+  [80, 40]
+]
+
+const colorForResource = {
+  'desert': 'black',
+  'wool': 'lightgreen',
+  'brick': 'darkred',
+  'lumber': 'green',
+  'grain': 'yellow',
+  'ore': 'lightgrey',
+}

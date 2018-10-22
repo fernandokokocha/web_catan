046c5a57bf8bfbf39b7081488c9a093455616706
Bartosz Krajka <krajka.bartosz@gmail.com>
2018-10-22 13:21:44 +0200

    Refactor map.js (better naming)

--- app/assets/javascripts/map/map.js
+++ app/assets/javascripts/map/map.js
@@ -1,14 +1,18 @@
 const shapeMap = () => {
-  const fieldNodes = document.getElementsByClassName("map-board")[0].childNodes;
-  const fields = Array.from(fieldNodes).filter(el => el.nodeName != '#text');
-  for (let i=0; i<19; i++) {
-    const field = fields[3*i];
-    const background = fields[3*i + 1];
-    const caption = fields[3*i + 2];
+  const mapNodes = document.getElementsByClassName("map-board")[0].childNodes;
+  const mapComponents = Array.from(mapNodes).filter(el => el.nodeName != '#text');
+
+  const fieldsCount = 19;
+  for (let i=0; i<fieldsCount; i++) {
+    const field = mapComponents[3*i];
+    const background = mapComponents[3*i + 1];
+    const caption = mapComponents[3*i + 2];
+
     const dims = dimsForField[i];
+    field.setAttribute("style", `position: absolute; left: ${dims[0]}px; top: ${dims[1]}px;`);
+
     const backgroundDims = dimsForBackground[i];
-    background.setAttribute("style", `position: absolute; top: ${backgroundDims[1]}px; left: ${backgroundDims[0]}px;`);
-    field.setAttribute("style", `position: absolute; top: ${dims[1]}px; left: ${dims[0]}px;`);
-    caption.setAttribute("style", `position: absolute; top: ${backgroundDims[1]}px; left: ${backgroundDims[0]}px;`);
+    background.setAttribute("style", `position: absolute; left: ${backgroundDims[0]}px; top: ${backgroundDims[1]}px;`);
+    caption.setAttribute("style", `position: absolute; left: ${backgroundDims[0]}px; top: ${backgroundDims[1]}px;`);
   }
 };

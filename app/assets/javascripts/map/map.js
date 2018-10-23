1c9fa033bf9483f0201025a05f897858888e3e86
Bartosz Krajka <krajka.bartosz@gmail.com>
2018-10-23 17:26:53 +0200

    Add preview of places at map

--- app/assets/javascripts/map/map.js
+++ app/assets/javascripts/map/map.js
@@ -1,13 +1,13 @@
-const shapeMap = () => {
+const shapeFields = () => {
   const fieldRootNode = document.getElementsByClassName("fields")[0];
   const fieldNodes = fieldRootNode.childNodes;
-  const mapComponents = Array.from(fieldNodes).filter(el => el.nodeName != '#text');
+  const fieldComponents = Array.from(fieldNodes).filter(el => el.nodeName != '#text');
 
   const fieldsCount = 19;
   for (let i=0; i<fieldsCount; i++) {
-    const field = mapComponents[3*i];
-    const background = mapComponents[3*i + 1];
-    const caption = mapComponents[3*i + 2];
+    const field = fieldComponents[3*i];
+    const background = fieldComponents[3*i + 1];
+    const caption = fieldComponents[3*i + 2];
 
     const dims = dimsForField[i];
     field.setAttribute("style", `position: absolute; left: ${dims[0]}px; top: ${dims[1]}px;`);
@@ -16,4 +16,25 @@ const shapeMap = () => {
     background.setAttribute("style", `position: absolute; left: ${backgroundDims[0]}px; top: ${backgroundDims[1]}px;`);
     caption.setAttribute("style", `position: absolute; left: ${backgroundDims[0]}px; top: ${backgroundDims[1]}px;`);
   }
+};
+
+const shapePlaces = () => {
+  const placeRootNode = document.getElementsByClassName("places")[0];
+  const placeNodes = placeRootNode.childNodes;
+  const placeComponents = Array.from(placeNodes).filter(el => el.nodeName != '#text');
+
+  const placeCount = 54;
+  for (let i=0; i<placeCount; i++) {
+    const place = placeComponents[2*i];
+    const background = placeComponents[2*i + 1];
+
+    const dims = dimsForPlace[i];
+    place.setAttribute("style", `position: absolute; left: ${dims[0]}px; top: ${dims[1]}px;`);
+    background.setAttribute("style", `position: absolute; left: ${dims[0]}px; top: ${dims[1]}px;`);
+  }
+};
+
+const shapeMap = () => {
+  shapeFields();
+  shapePlaces();
 };

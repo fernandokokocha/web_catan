ef25172e600dbeb82c1b9995b1a8cdec2f8d7393
Bartosz Krajka <bartosz.krajka@droidsonroids.pl>
2019-04-29 15:58:10 +0200

    WIP

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,26 +1,33 @@
 class GamesController < ApplicationController
   def new
-    render_game
+    render_game(generate_new_game)
   end
 
   def show
-    render_game
+    render_game(generate_new_game)
   end
 
   def update
     settlement_spot = Integer(params[:settlement][:spot_index])
     road_extension_spot = Integer(params[:road][:to])
+
+    # state = JSON.parse(params[:state])
+    state = JSON.parse(params[:state]).deep_symbolize_keys
+    pp state
+    game = GameDeserializer.new(state).call
+    # p params[:state].class
+    # game = generate_new_game
     @result = game.handle(SettleWithRoad.new(settlement_spot: settlement_spot, road_extension_spot: road_extension_spot))
-    render_game
+    render_game(game)
   end
 
   private
 
-  def game
-    @game ||= generate_new_game
-  end
+  # def game
+  #   @game ||= generate_new_game
+  # end
 
-  def render_game
+  def render_game(game)
     spots = [{ owner: nil, color: nil, builing: nil }] * 54
     game.settlements.each do |settlement|
       spots[settlement.spot_index - 1] = {
@@ -29,12 +36,15 @@ class GamesController < ApplicationController
         color: settlement.owner.color
       }
     end
+    pp GameSerializer.new(game).call.to_json
     render :game, locals: {
       error: error_message,
       tiles: game.tiles.sort_by { |tile| tile.index.to_i },
       places: spots,
       roads: game.roads,
-      state: GameSerializer.new(game).call,
+      state: GameSerializer.new(game).call.to_json,
+      turn: game.turn,
+      players: game.players,
       current_player: game.current_player
     }
   end
